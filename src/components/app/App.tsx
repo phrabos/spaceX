692b4a9b13dd50e09ffbebb3f8af8af126be2499
patrick hrabos <phrabos@gmail.com>
2021-10-23 13:18:36 -0700

    remove lorem ipsum

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,25 +6,6 @@ function App(): JSX.Element {
 	return (
 		<>
 			<ShipsContainer />
-			<div className="App">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum a
-				ipsum vel lorem gravida ultricies. Donec ac tempor sem. Pellentesque
-				habitant morbi tristique senectus et netus et malesuada fames ac turpis
-				egestas. Class aptent taciti sociosqu ad litora torquent per conubia
-				nostra, per inceptos himenaeos. Etiam a suscipit eros. Etiam aliquam leo
-				non sem auctor sodales. Ut blandit semper mattis. Duis placerat, diam
-				ultricies bibendum mattis, justo ex aliquet ligula, ornare posuere ante
-				est quis odio. Fusce vestibulum lobortis malesuada. Maecenas imperdiet
-				leo vitae dolor sodales, porttitor feugiat lorem fermentum. Nullam
-				ornare mi eu pharetra finibus. Curabitur interdum a tellus a lacinia.
-				Curabitur mattis dui ante, viverra luctus odio vulputate vitae. Nunc
-				mauris est, pretium nec urna nec, dapibus blandit urna. Phasellus
-				gravida tempor nisi. Vestibulum non nibh lacus. Nunc accumsan varius
-				imperdiet. Donec et cursus tortor, non fermentum urna. Donec finibus
-				mattis egestas. In varius mi at nibh tincidunt facilisis. Ut vitae lorem
-				dictum, iaculis eros eget, consectetur purus. Donec pretium vitae odio
-				et laoreet. Sed vel dolor dolor.
-			</div>
 		</>
 	);
 }
